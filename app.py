85b45cb1a5ff85624f01167a76824ba13d11914d
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-06-06 18:35:48 -0500

    try

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import json
+from bson.json_util import dumps
+from matplotlib import projections
+#from zmq import ROUTER
+from bson import json_util
+from flask_cors import CORS
+import sys
+
+# Create an instance of Flask
+app = Flask(__name__)
+CORS(app)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb+srv://mel518:databasepass@TestTrain.ppavz.mongodb.net/Project4?retryWrites=true&w=majority")
+
+@app.route("/")
+def home():
+    data = mongo.db.Data.find().limit(100)
+    list_cur = list(data)
+    json_data = jsonify(json_util.dumps(list_cur))
+    return json_data
+
+if __name__ == '__main__':
+    app.run(debug=True)
