95000c63215554a774f45bcd70f5320616f1edee
adkim <AndrewKim2022.1@U.NORTHWESTERN.EDU>
2022-06-07 21:25:15 -0500

    html corrected

--- js/andrewapp.js
+++ js/andrewapp.js
@@ -6,7 +6,6 @@ function init() {
     let dropdownMenu = d3.select("#selDataset");
     data.forEach((uniqueVarietyList) => {
       dropdownMenu.append('option').text(uniqueVarietyList)
-      // dropdownMenu.append('option').text(uniqueVarietyList).property("value",UniqueVarietyList);
     })
     //this is works because of Closure and Scope (Unique)
     // Start at wine option 0
@@ -18,6 +17,7 @@ function init() {
     //   var result = data[0].variety;
     // winelist(wineVariety);
   });
+  
 }
 
 // New Option for Drop Down, re-load all info
@@ -27,5 +27,31 @@ function init() {
 init();
 
 
+function init1() {
+  d3.json("http://127.0.0.1:5000/fighters").then(data => {
+
+    // Drop down menu creation
+    let dropdownMenu = d3.select("#selDataset1");
+    data.forEach((uniqueVarietyList) => {
+      dropdownMenu.append('option').text(uniqueVarietyList)
+    })
+    //this is works because of Closure and Scope (Unique)
+    // Start at wine option 0
+    var result = data[0];
+
+    // // for (let i = 0; i<data.length; i++){
+    // // console.log(data.country);
+    buildMetadata(result)
+    //   var result = data[0].variety;
+    // winelist(wineVariety);
+  });
+  
+}
+
+// New Option for Drop Down, re-load all info
+
+
+// Initialize the code
+init1();
 //
 
