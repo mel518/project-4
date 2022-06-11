1f7525a33437e05e463865980eb795dd8d83516f
adkim <AndrewKim2022.1@U.NORTHWESTERN.EDU>
2022-06-11 13:28:59 -0500

    UFC websites

--- js/andrewapp.js
+++ js/andrewapp.js
@@ -1,6 +1,6 @@
 
 function init() {
-  d3.json("http://127.0.0.1:5000/fighters").then(data => {
+  d3.json("https://ufcmatchdata.herokuapp.com/fighters").then(data => {
 
     // Drop down menu creation
     let dropdownMenu = d3.select("#selDataset");
@@ -28,7 +28,7 @@ init();
 
 
 function init1() {
-  d3.json("http://127.0.0.1:5000/fighters").then(data => {
+  d3.json("https://ufcmatchdata.herokuapp.com/fighters").then(data => {
 
     // Drop down menu creation
     let dropdownMenu = d3.select("#selDataset1");
