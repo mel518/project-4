5396547f8dd2cbdd422b222fbb50953763c8b310
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-06-10 19:35:05 -0500

    ryans

--- app.py
+++ app.py
@@ -25,6 +25,23 @@ model = joblib.load('fighter_classifier.h5')
 def home():
     return render_template('index.html')
 
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+    if prediction==0:
+        return render_template('index.html',
+                               prediction_text='Low chances of patient readmitted to hospital.'.format(prediction),
+                               )
+    else:
+        return render_template('index.html',
+                               prediction_text='High chances of patient readmitted to hospital'.format(prediction),
+                              )
+
 @app.route("/Data")
 def home():
     data = mongo.db.Data.find()
